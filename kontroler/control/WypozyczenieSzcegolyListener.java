b9b33ef53a6aa48d7423ba2c3bdcd3506b828a1e
DanielFurgał <192245@student.pwr.wroc.pl>
2014-01-29 11:06:26 +0100

    Dodanie metody setFields dla klas entity ustawiającej wszystkie pola, których nazwy zawierają się w tablicy haszowenej params

--- kontroler/control/WypozyczenieSzcegolyListener.java
+++ kontroler/control/WypozyczenieSzcegolyListener.java
@@ -3,11 +3,12 @@ package control;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JButton;
 
-import entity.Rezerwacja;
+import entity.*;
 import FramesComponets.Wypozyczenie;
 import FramesComponets.WypozyczenieSzczegoly;
 import model.Model;
@@ -32,34 +33,37 @@ public class WypozyczenieSzcegolyListener extends AbstractSingleControler implem
 				List<Rezerwacja> rezerwacje = (List<Rezerwacja>) SessionStore.INSTANCE.load("rezerwacjeList");
 				Integer currentIndex = (Integer) SessionStore.INSTANCE.load("indexRezerwacji");
 				Rezerwacja rezerwacja = rezerwacje.get(currentIndex);
-				System.out.println(panel.getFildsValue());
+				HashMap<String, Object> fildsValues = panel.getFildsValue();
 				System.out.println("Powrot");
+				System.out.println(rezerwacja);
 
-//				Klient klient = rez.getKlient();
-//				txtImi.setText(klient.getImie());
-//				Adres adres = klient.getAdres();
-//				txtNazwisko.setText(klient.getNazwisko());
-//				txtUlica.setText(adres.getUlica());
-//				txtKodPocztowy.setText(adres.getKodPocztowy());
-//				txtMiejscowo.setText(adres.getMiejscowosc());
-//				txtNrTelefonu.setText(klient.getNumerTelefonu());
-//				txtEmail.setText(klient.getEmail());
-//				if(!klient.getFirmy().isEmpty()){
-//					checkNajemFirmowy.setSelected(true);
-//					Firma firma = (Firma) klient.getFirmy().toArray()[0];
-//					txtNazwaFirmy.setText(firma.getNazwaFirmy());
-//					Adres adresFirmy = firma.getAdres();
-//					txtUlica_1.setText(adresFirmy.getUlica());
-//					txtKodPocztowy_1.setText(adresFirmy.getKodPocztowy());
-//					txtMiejscowo_1.setText(adresFirmy.getMiejscowosc());
-//					txtNrNip.setText(firma.getNIP());
-//				}
-//				txtKrajWydania.setSelectedItem(klient.getKrajWydaniaPrawaJazdy());
-//				datePanel.setDate(klient.getDataUrodzenia());
-//				txtNumerPrawaJazdy.setText(klient.getNumerPrawaJazdy());
-//				txtUwagi.setText(rez.getUwagi());
-//				int plecIndex = klient.getPlec().equals("M")? 0 : 1;
-//				comboPlec.setSelectedIndex(plecIndex);
+
+				Klient klient = rezerwacja.getKlient();
+				klient.setImie((String)fildsValues.get("imie"));
+				klient.setNazwisko((String)fildsValues.get("nazwisko"));
+				txtUlica.setText(adres.getUlica());
+				Adres adres = klient.getAdres();
+				adres.
+				txtKodPocztowy.setText(adres.getKodPocztowy());
+				txtMiejscowo.setText(adres.getMiejscowosc());
+				txtNrTelefonu.setText(klient.getNumerTelefonu());
+				txtEmail.setText(klient.getEmail());
+				if(!klient.getFirmy().isEmpty()){
+					checkNajemFirmowy.setSelected(true);
+					Firma firma = (Firma) klient.getFirmy().toArray()[0];
+					txtNazwaFirmy.setText(firma.getNazwaFirmy());
+					Adres adresFirmy = firma.getAdres();
+					txtUlica_1.setText(adresFirmy.getUlica());
+					txtKodPocztowy_1.setText(adresFirmy.getKodPocztowy());
+					txtMiejscowo_1.setText(adresFirmy.getMiejscowosc());
+					txtNrNip.setText(firma.getNIP());
+				}
+				txtKrajWydania.setSelectedItem(klient.getKrajWydaniaPrawaJazdy());
+				datePanel.setDate(klient.getDataUrodzenia());
+				txtNumerPrawaJazdy.setText(klient.getNumerPrawaJazdy());
+				txtUwagi.setText(rezerwacja.getUwagi());
+				int plecIndex = klient.getPlec().equals("M")? 0 : 1;
+				comboPlec.setSelectedIndex(plecIndex);
 			break;
 
 		default:
--- model/entity/Entity.java
+++ model/entity/Entity.java
@@ -1,13 +1,82 @@
 package entity;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+
 public class Entity {
 
 	@Override
 	public boolean equals(Object obj) {
-		if(obj.getClass().equals(this.getClass())){
+		if (obj.getClass().equals(this.getClass())) {
 			return this.toString().equals(obj.toString());
 		}
 		return false;
 	}
-	
+
+	// TODO Dodac testy
+
+	// tablica params zawiera w kluczu nazwe pola klasy a jako wartosc przyjmuje
+	// wartosc, która ma
+	// zostac ustawiona dla pola
+	public void setFields(HashMap<String, Object> params) {
+		try {
+			Class c = Class.forName(this.getClass().getCanonicalName());
+			setFieldsClass(c, params);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void setFieldsClass(Class c, HashMap<String, Object> params)
+			throws IllegalArgumentException, IllegalAccessException,
+			SecurityException, InvocationTargetException {
+
+		if (c != null) {
+			System.out.println(c.getCanonicalName());
+			Field[] fields = c.getDeclaredFields();
+			for (Field f : fields) {
+				String fieldName = f.getName();
+				if (params.containsKey(fieldName)) {
+					Object value = params.get(f.getName());
+					// pobranie gettera dla pola
+					java.lang.reflect.Method m;
+					try {
+						m = c.getMethod("set"
+								+ fieldName.substring(0, 1).toUpperCase()
+								+ fieldName.substring(1, fieldName.length()),
+								value.getClass());
+					} catch (NoSuchMethodException e) {
+						throw new IllegalArgumentException(c.getCanonicalName()
+								+ " metoda setter dla pola " + fieldName
+								+ " dla argumentu " + value.getClass()
+								+ " nie istniej");
+					}
+					// ustawienie wartoœci pola
+					m.invoke(this, value);
+				}
+			}
+			Class superClass = c.getSuperclass();
+			setFieldsClass(superClass, params);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		Klient a = new Klient();
+		HashMap<String, Object> params = new HashMap<String, Object>();
+		params.put("imie", "AAA");
+		a.setFields(params);
+		System.out.println(a);
+	}
 }
